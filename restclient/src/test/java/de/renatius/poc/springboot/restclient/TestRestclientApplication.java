37991d6a3c1daf9f3ff3f843d33f5b75b94560d6
René Six <info@renatius.de>
2025-04-16 10:56:35 +0200

    Adds basic restclient module

---
+++ restclient/src/test/java/de/renatius/poc/springboot/restclient/TestRestclientApplication.java
@@ -0,0 +1,6 @@
+package de.renatius.poc.springboot.restclient;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestclientApplication {}
--- src/test/java/de/renatius/poc/springboot/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package de.renatius.poc.springboot;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
