8ed193acc40c62f07104b62bc4022a6eff0dd028
René Six <info@renatius.de>
2026-09-12 21:00:08 +0200

    feat(deps): add Flyway support and configure spring-boot-maven-plugin

--- data/src/test/java/de/renatius/poc/springboot/data/repository/RepositoryIntegrationTest.java
+++ data/src/test/java/de/renatius/poc/springboot/data/repository/RepositoryIntegrationTest.java
@@ -17,12 +17,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest(classes = TestDataApplication.class)
+@Transactional
 class RepositoryIntegrationTest {
 
   @Container @ServiceConnection
